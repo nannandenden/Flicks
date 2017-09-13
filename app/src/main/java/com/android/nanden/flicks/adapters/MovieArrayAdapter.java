0110891c3f81612fbb419bb48f5be07001ae26d1
Nan Den <nannandenden@gmail.com>
2017-09-13 12:04:19 -0700

    add ViewHolder pattern to improve the list view performance

--- app/src/main/java/com/android/nanden/flicks/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/android/nanden/flicks/adapters/MovieArrayAdapter.java
@@ -17,7 +17,17 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import static com.android.nanden.flicks.R.id.ivBackDrop;
+import static com.android.nanden.flicks.R.id.ivPoster;
+
 public class MovieArrayAdapter extends ArrayAdapter<Movie>{
+
+    private static class ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+        ImageView ivBackDrop;
+    }
 
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
@@ -29,35 +39,45 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie>{
         // get the data item for position
         Movie movie = getItem(position);
 
+        ViewHolder viewHolder;  // view lookup cache stored in tag
+
         // check to see if existing view being reused
-        if (convertView == null) { // if it's null means there is no view that can be reused so
-            // we want to inflate the custom view
+        if (convertView == null) { // if it's null means there is no view that can be reused
+            viewHolder = new ViewHolder();
+            // thus, we want to inflate the custom view
             LayoutInflater inflater = LayoutInflater.from(getContext()); // context is passed by
             // the MovieArrayAdapter constructor
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
             // use R.layout.item_movie to inflate the data
             // parent values for root of the returned hierarchy
             // false Whether the inflated hierarchy should be attached to the root parameter
+            viewHolder.tvTitle = convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = convertView.findViewById(R.id.tvOverview);
+            viewHolder.ivPoster = convertView.findViewById(ivPoster);
+            viewHolder.ivBackDrop = convertView.findViewById(ivBackDrop);
+            // cache the viewHolder object inside the fresh view
+            // Sets the tag associated with this view. Tags can also be used to store data within
+            // a view without resorting to another data structure.
+            convertView.setTag(viewHolder);
+        } else {
+            // view is begin recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         // populate the items
-        ImageView ivPoster = convertView.findViewById(R.id.ivPoster);
-        ImageView ivBackDrop = convertView.findViewById(R.id.ivBackDrop);
-        TextView tvTitle = convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = convertView.findViewById(R.id.tvOverview);
-
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
+        // populate the data from the data object via the viewHolder object into the template view
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
 
         int orientation = getContext().getResources().getConfiguration().orientation;
         if(orientation == Configuration.ORIENTATION_PORTRAIT) {
             // clear out image from convertView. need to clear inside the orientation condition
             // since different orientation will not see this imageView
-            ivPoster.setImageResource(0);
-            Picasso.with(getContext()).load(movie.getPosterPath()).into(ivPoster);
+            viewHolder.ivPoster.setImageResource(0);
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivPoster);
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            ivBackDrop.setImageResource(0);
-            Picasso.with(getContext()).load(movie.getBackDropPath()).into(ivBackDrop);
+            viewHolder.ivBackDrop.setImageResource(0);
+            Picasso.with(getContext()).load(movie.getBackDropPath()).into(viewHolder.ivBackDrop);
         }
 
         return convertView;
