4e20f05bdad7923a623c6e05b8b7e76a63efa4fb
Nan Den <nannandenden@gmail.com>
2017-09-18 13:36:11 -0700

    handle network call in movie client

--- app/src/main/java/com/android/nanden/flicks/activitites/MoviesActivity.java
+++ app/src/main/java/com/android/nanden/flicks/activitites/MoviesActivity.java
@@ -9,8 +9,7 @@ import android.util.Log;
 import com.android.nanden.flicks.R;
 import com.android.nanden.flicks.adapters.MovieAdapter;
 import com.android.nanden.flicks.models.Movie;
-import com.android.nanden.flicks.utils.Constants;
-import com.loopj.android.http.AsyncHttpClient;
+import com.android.nanden.flicks.network.MovieClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -26,6 +25,7 @@ public class MoviesActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     MovieAdapter movieAdapter;
     RecyclerView recyclerView;
+    MovieClient client;
 
 
     @Override
@@ -39,34 +39,35 @@ public class MoviesActivity extends AppCompatActivity {
         movieAdapter = new MovieAdapter(this, movies);
         recyclerView.setAdapter(movieAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        fetchMovies();
+    }
 
-        AsyncHttpClient client = new AsyncHttpClient();
-
-        client.get(Constants.URL, new JsonHttpResponseHandler(){
+    private void fetchMovies() {
+        client = new MovieClient();
+        client.getMovies(new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
-                // extract the response from the response and stores in json array
                 JSONArray movieJsonResult = null;
 
                 try {
                     movieJsonResult = response.getJSONArray("results");
-                    // since we instantiate movie ArrayList already, use addAll method to add the
-                    // movie objects into movie ArrayList this is a easy mistake to make
+
                     movies.addAll(Movie.fromJSONArray(movieJsonResult));
-                    // do not forget to notify the adapter
                     movieAdapter.notifyDataSetChanged();
-                    Log.d("Debug", movies.toString());
-                    // convert json object to movies object so we can manipulate
+
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    Log.d("LOG_TAG", "ERROR: " + e.getMessage());
                 }
+
 
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
+                Log.d("LOG_TAG", "Request failed. status code: " + statusCode + "\t" +
+                        responseString);
             }
         });
     }
