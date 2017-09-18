db21a234449fcc4fb1822ec630a0d4fd7d3377b6
Nan Den <nannandenden@gmail.com>
2017-09-18 16:05:45 -0700

    Add butterknife to reduce view boilerplate

--- app/src/main/java/com/android/nanden/flicks/adapters/MovieAdapter.java
+++ app/src/main/java/com/android/nanden/flicks/adapters/MovieAdapter.java
@@ -3,6 +3,7 @@ package com.android.nanden.flicks.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +16,15 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import butterknife.BindDrawable;
+import butterknife.ButterKnife;
+
 public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private List<Movie> movies;
     private Context context;
+    @BindDrawable(R.drawable.placeholder)
+    Drawable placeHolder;
     public MovieAdapter(Context context, List<Movie> movies) {
         this.context = context;
         this.movies = movies;
@@ -38,9 +44,10 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         }
         return -1;
     }
-
+    // Called when RecyclerView needs a new RecyclerView.ViewHolder of the given type to represent an item.
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ButterKnife.bind(this, parent);
         RecyclerView.ViewHolder viewHolder = null;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         if (viewType == StarCount.POPULAR.ordinal()) {
@@ -68,7 +75,7 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private void configViewHolderPopular(ViewHolderPopular vhPopular, int position) {
         vhPopular.getIvFullBackDrop().setImageResource(0);
         Picasso.with(context).load(movies.get(position).getBackDropPath())
-                .placeholder(R.drawable.placeholder)
+                .placeholder(placeHolder)
                 .into(vhPopular.getIvFullBackDrop());
         int orientation = context.getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -80,7 +87,7 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private void configViewHolderSoso(ViewHolderSoso vhSoso, int position) {
         vhSoso.getIvPoster().setImageResource(0);
         Picasso.with(context).load(movies.get(position).getPosterPath())
-                .placeholder(R.drawable.placeholder)
+                .placeholder(placeHolder)
                 .into(vhSoso.getIvPoster());
         vhSoso.getTvTitle().setText(movies.get(position).getOriginalTitle());
         vhSoso.getTvOverView().setText(movies.get(position).getOverview());
--- app/src/main/java/com/android/nanden/flicks/adapters/ViewHolderSoso.java
+++ app/src/main/java/com/android/nanden/flicks/adapters/ViewHolderSoso.java
@@ -1,5 +1,6 @@
 package com.android.nanden.flicks.adapters;
 
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,17 +8,21 @@ import android.widget.TextView;
 
 import com.android.nanden.flicks.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ViewHolderSoso extends RecyclerView.ViewHolder {
 
-    private TextView tvTitle;
-    private TextView tvOverView;
-    private ImageView ivPoster;
+    @Nullable
+    @BindView(R.id.tvTitle) TextView tvTitle;
+    @Nullable
+    @BindView(R.id.tvOverview) TextView tvOverView;
+    @Nullable
+    @BindView(R.id.ivPoster) ImageView ivPoster;
 
     public ViewHolderSoso(View itemView) {
         super(itemView);
-        tvTitle = itemView.findViewById(R.id.tvTitle);
-        tvOverView = itemView.findViewById(R.id.tvOverview);
-        ivPoster = itemView.findViewById(R.id.ivPoster);
+        ButterKnife.bind(this, itemView);
     }
 
     public TextView getTvTitle() {
